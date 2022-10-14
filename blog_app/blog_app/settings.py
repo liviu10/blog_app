e80adf2a4e23ab617351a20f821d735a72d80e05
Liviu Voica <voica.liviu10@gmail.com>
2022-10-14 18:45:23 +0300

    Rename project to blog_app

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'blog.urls'
+ROOT_URLCONF = 'blog_app.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blog.wsgi.application'
+WSGI_APPLICATION = 'blog_app.wsgi.application'
 
 
 # Database
